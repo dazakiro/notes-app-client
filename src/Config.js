74bd9daa6b2491d4e941e650f672b0d6a1002ace
Zack <dazakiro@gmail.com>
2020-10-09 16:21:40 +0200

    setting up front app

---
+++ src/Config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "note-app-uploads-zack"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://pvimljecqi.execute-api.us-east-2.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_TiGzzyx9Y",
+      APP_CLIENT_ID: "aup34sav1tv148c9t2bd05vg",
+      IDENTITY_POOL_ID: "us-east-2:a790f8a2-993b-437b-bbfa-a4070fd852a5"
+    }
+  };
