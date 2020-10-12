05b41510e3b4ff66504afec357fa590919af23d8
Zack <dazakiro@gmail.com>
2020-10-12 23:47:22 +0200

    finilazing app

---
+++ src/containers/Login.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import {  FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import {Auth} from "aws-amplify";
+import "./Login.css";
+import {useAppContext} from "../libs/contextLib";
+import { useFormFields } from "../libs/hooksLib";
+import LoaderButton from "../components/LoaderButton";
+import {onError} from "../libs/errorLib";
+
+export default function Login() {
+    const [fields, handleFieldChange] = useFormFields({
+        email: "",
+        password: ""
+      });
+  const { userHasAuthenticated } = useAppContext();
+  
+  const [isLoading, setIsLoading] = useState(false);
+
+  function validateForm() {
+    return fields.email.length > 0 && fields.password.length > 0;
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setIsLoading(false);
+
+    try {
+        await Auth.signIn(fields.email, fields.password);
+        userHasAuthenticated(true);
+        
+      } catch (e) {
+        onError(e);
+        setIsLoading(true);
+      }
+  }
+
+  return (
+    <div className="Login">
+      <form onSubmit={handleSubmit}>
+        <FormGroup controlId="email" bsSize="large">
+          <ControlLabel>Email</ControlLabel>
+          <FormControl
+            autoFocus
+            type="email"
+            value={fields.email}
+            onChange={handleFieldChange}
+          />
+        </FormGroup>
+        <FormGroup controlId="password" bsSize="large">
+          <ControlLabel>Password</ControlLabel>
+          <FormControl
+            value={fields.password}
+            onChange={handleFieldChange}
+            type="password"
+          />
+        </FormGroup>
+        <LoaderButton
+  block
+  type="submit"
+  bsSize="large"
+  isLoading={isLoading}
+  disabled={!validateForm()}
+>
+  Login
+</LoaderButton>
+
+      </form>
+    </div>
+  );
+}
